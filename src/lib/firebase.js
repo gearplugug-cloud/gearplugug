387cf7f593abb5f9b7e53bee1e1d62926baefcf9
fathers arise <gearplugug@gmail.com>
2026-08-17 15:41:27 +0300

    udate fire base

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyDGc1Cy07Cdrb1xi0-QeS1qRe9I1xJXlxo",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "gear-plug.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "gear-plug",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "gear-plug.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "565997758146",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:565997758146:web:c3c2e143d2601268f6bd5b"
 };
 
 // Initialize Firebase
